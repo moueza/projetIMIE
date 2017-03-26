be327fc852ce1548612352fd5001db7761c1ff51
Terni <Terni@DESKTOP-HO3324I.home>
2017-03-26 20:03:37 +0200

    modif homecontroller

--- GestionTournoi/src/main/java/presentation/HomeController.java
+++ GestionTournoi/src/main/java/presentation/HomeController.java
@@ -9,40 +9,39 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class HomeController {
-	
+
 	private String title;
 	private String description;
 	private String entete;
 	private List<String> lesJoueurs;
 	private List<String> list;
-	
+
 	public HomeController() {
 
 	}
 
 	public String affichagePageAjoutJoueur() {
-		
+
 		title = "Enregistrement d'un nouveau joueur";
-		description = "Enregistrer un nouveau joueur en indiquant son nom, son prénom," + 
-		"son sexe et sa nationalité.";
-		entete  = "Liste des joueurs";
+		description = "Enregistrer un nouveau joueur en indiquant son nom, son prénom," + "son sexe et sa nationalité.";
+		entete = "Liste des joueurs";
 		list = new ArrayList<String>();
 		list.add("novak djokovic");
 		return "player";
 	}
 
 	public String affichagePageEnregistrerArbitre() {
-		
+
 		title = "Enregistrement des arbitres";
 		description = "Enregistrer un nouvel arbitre en indiquant son nom,son "
-						+"prénom, sa nationalité et son niveau(chaise ou ligne).";
+				+ "prénom, sa nationalité et son niveau(chaise ou ligne).";
 		entete = "Liste des arbitres";
 
 		return "arbitre";
 	}
 
 	public String affichagePageEnregistrerCourt() {
-		
+
 		title = "Enregistrer des courts";
 		description = "Enregistrer un nouveau court en indiquant son nom";
 		entete = "Liste des courts";
@@ -50,16 +49,15 @@ public class HomeController {
 	}
 
 	public String affichagePagePlanifierMatch() {
-		
+
 		title = "Planifier des matchs";
-		description = "Pour créer un match, sélectionne deux joueurs, un arbitre,"
-						+ "un court et une date";
-		
+		description = "Pour créer un match, sélectionne deux joueurs, un arbitre," + "un court et une date";
+
 		lesJoueurs = new ArrayList<String>();
-		
+
 		lesJoueurs.add("toto");
 		lesJoueurs.add("toto");
-		
+
 		return "planning";
 	}
 
@@ -69,7 +67,7 @@ public class HomeController {
 	}
 
 	public String deconnexion() {
-		
+
 		return "deconnection";
 	}
 
@@ -112,7 +110,5 @@ public class HomeController {
 	public void setList(List<String> list) {
 		this.list = list;
 	}
-	
-	
-	
+
 }
